d110640044acc3b493a3ea287f15d719f89af3fe
UncleLukie <lukehewitt@smashit.games>
2023-03-29 08:28:33 +1000

    Cart work, fixed total showing correctly

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -40,6 +40,21 @@ function fetchTextColors() {
   });
 }
 
+function calculateCartTotal() {
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let total = 0;
+
+  cart.forEach((cup) => {
+    let cupPrice = cup.price * cup.quantity;
+    let aestheticPrice = cup.aesthetic ? cup.aesthetic.price * cup.quantity : 0;
+    let textColorPrice = cup.text_color ? cup.text_color.price * cup.quantity : 0;
+    total += cupPrice + aestheticPrice + textColorPrice;
+  });
+
+  return total;
+}
+
+
 
 function fetchCups() {
   return $.get("/api/cups", function (data) {
@@ -56,21 +71,28 @@ let cart = {
 
 function addToCart(cupId, customizedCup = null) {
   let cup = customizedCup ? customizedCup : getCupById(cupId);
-  console.log("Cup:", cup);
 
   if (!cup) {
     return;
   }
 
-  if (cup.customizable && !customizedCup) {
-    selectedCup = cup;
-    openCustomizationModal();
-  } else {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  if (cup.customizable) {
+    cup.quantity = 1; // Add this line to set the quantity property for the customizable cup
     cart.push(cup);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    populateCart();
+  } else {
+    const existingCup = cart.find(item => item.id === cup.id && !item.customizable);
+    if (existingCup) {
+      existingCup.quantity += 1;
+    } else {
+      cup.quantity = 1;
+      cart.push(cup);
+    }
   }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  populateCart();
 }
 
 function getCupById(cupId) {
@@ -93,9 +115,27 @@ function showCart() {
     const textColorText = cup.text_color ? cup.text_color.name : '';
     const textContentText = cup.text_content || '';
     const customizations = [aestheticText, textColorText, textContentText].filter(Boolean).join(" - ");
-    cartItems += `<li>${cup.name}${customizations ? ' - ' + customizations : ''}</li>`;
+    const quantityControl = cup.customizable
+      ? ''
+      : `
+        <button class="btn btn-sm btn-outline-secondary update-quantity" data-cup-id="${cup.id}" data-action="decrement">-</button>
+        <input type="text" class="form-control cart-item-quantity" value="${cup.quantity}" readonly />
+        <button class="btn btn-sm btn-outline-secondary update-quantity" data-cup-id="${cup.id}" data-action="increment">+</button>
+      `;
+    cartItems += `
+      <li class="d-flex justify-content-between align-items-center">
+        <span>${cup.name}${customizations ? ' - ' + customizations : ''}</span>
+        <span class="d-flex align-items-center">
+          ${quantityControl}
+          <span class="ms-2">$${(cup.customizable && cup.total_price ? cup.total_price : cup.price * cup.quantity).toFixed(2)}</span>
+        </span>
+      </li>
+    `;
   });
 
+  // Call calculateCartTotal and update the total price in the cart modal
+  let totalPrice = calculateCartTotal();
+  $("#cart-total").text(totalPrice.toFixed(2));
   $("#cart-items").html(cartItems);
   $("#cartModal").modal("show");
 }
@@ -109,14 +149,14 @@ function openCustomizationModal() {
   // Populate aesthetic options
   let aestheticOptions = "";
   aesthetics.forEach((aesthetic) => {
-    aestheticOptions += `<option value="${aesthetic.id}" data-image="${aesthetic.image_url}">${aesthetic.name} (${aesthetic.price.toFixed(2)})</option>`;
+    aestheticOptions += `<option value="${aesthetic.id}" data-image="${aesthetic.image_url}" data-price="${aesthetic.price}">${aesthetic.name} (${aesthetic.price.toFixed(2)})</option>`;
   });
   $("#aesthetic-selector").html(aestheticOptions);
 
   // Populate text color options
   let textColorOptions = "";
   text_colors.forEach((text_color) => {
-    textColorOptions += `<option value="${text_color.id}" data-image="${text_color.image_url}">${text_color.color} (${text_color.price.toFixed(2)})</option>`;
+    textColorOptions += `<option value="${text_color.id}" data-image="${text_color.image_url}" data-price="${text_color.price}">${text_color.color} (${text_color.price.toFixed(2)})</option>`;
   });
   $("#text-color-selector").html(textColorOptions);
 
@@ -133,6 +173,11 @@ $(document).ready(function () {
     aesthetics = data.aesthetics;
   });
 
+  function calculateTotalPrice(cup, aestheticPrice, textColorPrice) {
+    return cup.price + aestheticPrice + textColorPrice;
+  }
+
+
   function clearCart() {
   localStorage.removeItem("cart");
   populateCart();
@@ -147,7 +192,14 @@ $(document).ready(function () {
   // Event listener for "Add to Cart" buttons
   $("#cups-container").on("click", ".add-to-cart", function () {
     let cupId = $(this).data("cup-id");
-    addToCart(cupId);
+    let cup = getCupById(cupId);
+
+    if (cup.customizable) {
+      selectedCup = cup;
+      openCustomizationModal();
+    } else {
+      addToCart(cupId);
+    }
   });
 
 
@@ -178,24 +230,74 @@ $(document).ready(function () {
     let selectedTextColor = $("#text-color-selector option:selected");
     let textContent = $("#text-content").val();
 
+    let aestheticPrice = parseFloat(selectedAesthetic.text().match(/\(([^)]+)\)/)[1]);
+    let textColorPrice = parseFloat(selectedTextColor.text().match(/\(([^)]+)\)/)[1]);
+    let totalPrice = cup.price + aestheticPrice + textColorPrice;
+
     cup.aesthetic_id = parseInt(selectedAesthetic.val());
     cup.aesthetic = {
       name: selectedAesthetic.text(),
-      image_url: selectedAesthetic.data("image")
+      image_url: selectedAesthetic.data("image"),
+      price: aestheticPrice
     };
     cup.text_color_id = parseInt(selectedTextColor.val());
     cup.text_color = {
       name: selectedTextColor.text(),
-      image_url: selectedTextColor.data("image")
+      image_url: selectedTextColor.data("image"),
+      price: textColorPrice
     };
     cup.text_content = textContent;
+    cup.total_price = totalPrice;
 
     addToCart(cup.id, cup);
-    $("#customizationModal").modal("hide")
-    });
+    $("#customizationModal").modal("hide");
+  });
 
     $("#clear-cart-button").on("click", function () {
     clearCart();
     });
+
+    $("#cart-items").on("click", ".plus", function () {
+      let index = $(this).closest("li").index();
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      cart[index].quantity++;
+      localStorage.setItem("cart", JSON.stringify(cart));
+      showCart();
+    });
+
+    $("#cart-items").on("click", ".minus", function () {
+      let index = $(this).closest("li").index();
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      cart[index].quantity--;
+
+      if (cart[index].quantity === 0) {
+        cart.splice(index, 1);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      showCart();
+    });
+    $("#cart-items").on("click", ".update-quantity", function () {
+      let cupId = $(this).data("cup-id");
+      let action = $(this).data("action");
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      let existingCartItem = cart.find((item) => item.id === cupId && !item.customized);
+
+      if (existingCartItem) {
+        if (action === "increment") {
+          existingCartItem.quantity++;
+        } else if (action === "decrement") {
+          existingCartItem.quantity--;
+          if (existingCartItem.quantity <= 0) {
+            cart = cart.filter((item) => !(item.id === cupId && !item.customized));
+          }
+        }
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+        populateCart();
+        showCart();
+      }
+    });
+
     populateCart();
 });
